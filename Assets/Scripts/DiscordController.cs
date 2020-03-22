16a460d4c8238cbfa9097c4132fa78720cc5f9a1
iTakeshi <itakeshi.net@gmail.com>
2020-03-23 00:36:35 +0900

    connect to Discord, Lobby, and then Voice

---
+++ Assets/Scripts/DiscordController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+
+public class DiscordController : MonoBehaviour {
+
+    private long ClientId;
+
+    private Discord.Discord discord;
+
+    void Start () {
+        discord = new Discord.Discord(ClientId, (System.UInt64)Discord.CreateFlags.Default);
+
+        var userManager = discord.GetUserManager();
+        userManager.OnCurrentUserUpdate += () => {
+            var currentUser = userManager.GetCurrentUser();
+            Debug.LogFormat("current user id: {0}", currentUser.Id);
+        };
+
+        var lobbyManager = discord.GetLobbyManager();
+        GetOrCreateLobby(lobbyManager, (lobby) => {
+            lobbyManager.ConnectVoice(lobby.Id, (result) => {
+                if (result == Discord.Result.Ok) {
+                    Debug.LogFormat("voice connected: {0}", lobby.Id);
+                }
+            });
+        });
+    }
+
+    void GetOrCreateLobby (Discord.LobbyManager lobbyManager, Action<Discord.Lobby> callback) {
+        var search = lobbyManager.GetSearchQuery();
+        search.Limit(1);
+        lobbyManager.Search(search, (result) => {
+            if (result == Discord.Result.Ok) {
+                if (lobbyManager.LobbyCount() == 1) {
+                    var lobbyId = lobbyManager.GetLobbyId(0);
+                    var lobby = lobbyManager.GetLobby(lobbyId);
+                    callback(lobby);
+                } else {
+                    var txn = lobbyManager.GetLobbyCreateTransaction();
+                    txn.SetCapacity(100);
+                    txn.SetType(Discord.LobbyType.Public);
+                    lobbyManager.CreateLobby(txn, (Discord.Result result1, ref Discord.Lobby lobby) => {
+                        if (result1 == Discord.Result.Ok) {
+                            Debug.LogFormat("new lobby created with password `{0}`", lobby.Secret);
+                            callback(lobby);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    void Update () {
+        discord.RunCallbacks();
+    }
+
+}
